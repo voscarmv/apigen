5911c3b6de1f50ac989f84becb58545dfcd4ea54
Oscar Mier <voscarmv@gmail.com>
2025-12-18 18:45:32 -0600

    To publish NPM

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { AiMessageStoreBackend } from "./server.js";
-import 'dotenv/config';
-
-if (!process.env.DATABASE_URL) {
-    throw Error('DATABASE_URL undefined');
-}
-
-const server = new AiMessageStoreBackend({
-    dbUrl: process.env.DATABASE_URL,
-    port: 3000
-});
-
-(async () => {
-    console.log("Migrate DB");
-    await server.migrate()
-    console.log("Done Migrating DB. Start server...");
-    server.listen();
-})();
+export { type StoreBackend } from "./server.js";
+export { type MessageStoreBackendParams } from "./server.js";
+export { AiMessageStoreBackend } from "./server.js"
